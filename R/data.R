cebc33295e107fa34cae8a0eebbd5b73416fabc7
Heike Hofmann <hhofmann4@unl.edu>
2026-04-27 12:49:42 -0500

    documenting the data set

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' List of five diamonds datasets from kaggle
+#'
+#' The `diamonds` dataset is well-known as part of the ggplot2 package.
+#' The kaggle platform hosts an astonishing 176 datasets named `diamonds` ([search result](https://www.kaggle.com/search?q=diamonds+in%3Adatasets)
+#' from 2026, Apr 26). Five of these datasets are included in the
+#' `kgl_diamonds` list. Each item of the list contains one dataset, named
+#' after their respective kaggle author.
+"kgl_diamonds"
